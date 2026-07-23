fb20320399e1e7688d5172c34e4e94fb0566e72e
Valentin Palamara <valentin.palamara@hotmail.com>
2026-07-23 16:48:06 +0200

    ajout des commentaires pour savoir quoi faire

--- stranger.sh
+++ stranger.sh
@@ -5,3 +5,12 @@
 #last modif : 22/07/26
 ############################
 
+# l'idée c'est de refaire la scène des monty python
+#avec le vieux qui pose des questions sur un pont
+#
+#utiliser un random pour savoir si la suite de questions
+#sera facile ou pas (utiliser pair impair)
+#
+#faire exploser le vieux si une des réponses input
+#correspond à afrique ou europe
+
